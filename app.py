508efd019d77b89c59010f61aaafa7e8644cf503
iGera97 <pesgmm16@gmail.com>
2023-02-21 22:53:26 -0600

    request_response_body

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name : str
+    last_name : str
+    age : int
+    hair_color : Optional[str] = None
+    is_married : Optional[bool] = None
 
 #Path Operation
 @app.get("/")#Path Operaion decorator
 def home():#Path operation function
     return {"Hello" : "World"}
 
-#Path Parameter
-@app.get("/ids/{id_}")
-def get_id(id_ : int):
-    return {
-        "Id" : id_
-    }
-
-#Query Parameter
-@app.get("/ids/{id_}/details")
-def get_id(id_ : int,age : Optional[int] = None):
-    
-    return {
-        "Id" : id_,
-        "age" : age
-    }
+#Request and response body
+@app.post("/person/new")
+def create_person(person : Person = Body()):
+    return person
