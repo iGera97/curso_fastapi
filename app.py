c568f149242c58338dd2cf30e2ec9cf34fd6c95d
iGera97 <pesgmm16@gmail.com>
2023-02-21 23:14:54 -0600

    validations_query

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -21,3 +21,11 @@ def home():#Path operation function
 @app.post("/person/new")
 def create_person(person : Person = Body()):
     return person
+
+#Query Validation
+@app.post("/person/detail")
+def show_detail(
+    name : Optional[str] = Query(None,min_length=1,max_length=50,title="Nombre de las persona",description="ingresa tu nombre mi compa"),
+    age : int = Query(ge=0)
+    ):
+    return {name : age}
