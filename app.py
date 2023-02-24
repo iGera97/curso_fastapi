3e3ec6cc052317ea8991c10518276a5feb3e9a47
iGera97 <pesgmm16@gmail.com>
2023-02-23 23:07:50 -0600

    validation_body

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ class Person(BaseModel):
     hair_color : Optional[str] = None
     is_married : Optional[bool] = None
 
+class Location(BaseModel):
+    city : str
+    state : str
+    country : str
+
 #Path Operation
 @app.get("/")#Path Operaion decorator
 def home():#Path operation function
@@ -23,15 +28,32 @@ def create_person(person : Person = Body()):
     return person
 
 #Query Validation
-@app.post("/person/detail")
+@app.get("/person/detail")
 def show_detail(
     name : Optional[str] = Query(None,min_length=1,max_length=50,title="Nombre de las persona",description="ingresa tu nombre mi compa"),
     age : int = Query(ge=0)
     ):
     return {name : age}
 
-@app.post("/person/detail/{person_id}")
+#Path Validation
+@app.get("/person/detail/{person_id}")
 def show_detail(
-    person_id : int = Path(None,gt=0,title="id de la persona",description="ingresa el id mi compa")
+    person_id : int = Path(gt=0,title="id de la persona",description="ingresa el id mi compa")
     ):
     return {"id" : person_id}
+
+#Request Body
+@app.put("/person/{person_id}")
+def update_person(
+    person_id : int = Path(
+        gt=0,
+        title="id de la persona",
+        description="ingresa el id mi compa"),
+    person : Person = Body(),
+    location : Location = Body()
+    ):
+
+    result = person.dict()
+    result.update(location.dict())
+
+    return result
